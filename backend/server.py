a6ebac4af4b16bf7c46d73c5c2c5f1a6b70af9f3
Kieran Llarena <baymax709@gmail.com>
2024-11-24 02:14:21 -0500

    feat: transport data from frontend to backend

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
 
-@app.get("/")
-def root():
+@app.post("/run")
+def run(
+        repo: str = Form(...),
+        app_type: str = Form(...)
+        ):
+    print(repo, app_type)
+
     return "Hello World"
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
